952a433dfaca3dc35730d9a823c4fbb2878fa89e
greeninja <nickcampion88@yahoo.co.uk>
2016-04-19 11:28:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ynny | /usr/bin/phoronix-test-suite batch-setup
-/usr/bin/phoronix-test-suite phoromatic.connect phoronix-benchmarks.perf-paas.fasthosts.net.uk:80/$CODE
+/usr/bin/phoronix-test-suite phoromatic.connect benchmark.paas.fasthosts.co.uk:80/$CODE
 
 
 
